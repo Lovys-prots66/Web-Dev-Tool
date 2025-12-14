4cd2d9a9500caac109428ff0b8f5097c72aea776
Lovys-prots66 <lovys6027@gmail.com>
2025-12-14 21:38:57 +0100

    add making of project with default content

---
+++ default/defaultSettings.cjs
@@ -0,0 +1,9 @@
+const fileNames = {
+    HTML : "index.html",
+    CSS : "style.css",
+    JAVASCRIPT : "script.js" 
+}
+
+const folderName = "project";
+
+module.exports = {fileNames, folderName}
--- index.cjs
+++ index.cjs
@@ -1,15 +1,63 @@
-const { userInfo } = require('os');
 const fs = require("fs/promises");
 const readLine = require("readline/promises");
+const path = require('path');
+const {fileNames, folderName} = require("./default/defaultSettings.cjs");
+const os = require('os');
 
-const path = require('path')
+console.log(fileNames);
+console.log(folderName);
+// const path = require('path')
 
-console.log(path.join(userInfo().homedir, 'Desktop'));
+// console.log(path.join(userInfo().homedir, 'Desktop'));
 
 // console.log(os.uptime() % (60 * 60));
 
-console.log(path.join(__dirname, "gg.txt"));
+// console.log(path.join(__dirname, "gg.txt"));
 
-console.log(path.resolve('.cjs'));
+// console.log(path.resolve('.cjs'));
 
-console.log(path.resolve("Desktop"));
+// console.log(path.resolve("Desktop"));
+
+function askInput(question){
+    return new Promise((resolve, reject) => {
+        const rl = readLine.createInterface({
+            input : process.stdin,
+            output : process.stderr
+        });
+
+        rl.question(question, (value) => {
+            if(!value.trim()){
+                resolve("default");
+            }
+
+            resolve(value);
+        });
+    })
+}
+
+async function makeFiles(){
+
+    try{
+        const defaultHTML = await fs.readFile("./default/fileContents/default.html");
+        const defaultCSS = await fs.readFile("./default/fileContents/default.css");
+        const defaultJS = await fs.readFile("./default/fileContents/default.js");
+        
+        await Promise.all([
+            await fs.mkdir(path.join(os.homedir(), 'Desktop', folderName), {recursive : true}),
+            await fs.writeFile((path.join(os.homedir(), 'Desktop', folderName, fileNames.HTML)), defaultHTML),
+            await fs.writeFile((path.join(os.homedir(), 'Desktop', folderName, fileNames.CSS)), defaultCSS),
+            await fs.writeFile((path.join(os.homedir(), 'Desktop', folderName, fileNames.JAVASCRIPT)), defaultJS)
+        ]);    
+        
+
+    }catch(err){
+        console.error(err.message);
+    }
+
+
+
+
+}
+
+
+makeFiles();
