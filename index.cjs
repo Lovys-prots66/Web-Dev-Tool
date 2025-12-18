a6f83659af8940164d6aa6aae9d4ca483361dfd7
Lovys-prots66 <lovys6027@gmail.com>
2025-12-18 21:30:33 +0100

    Add prompting for custom dir and project name

--- index.cjs
+++ index.cjs
@@ -5,8 +5,22 @@ const os = require('os');
 
 const {fileNames, folderName} = require("./default/defaultSettings.cjs");
 
+// function for checking the existence of a directory
+async function dirExists(dir){
 
+    try{
+        const stat = await fs.stat(dir);
 
+        if(stat.isDirectory()){
+            console.log("Project already exists");
+            return true;
+        }
+    }catch(err){
+        return true;
+    }
+} 
+
+// functions for requesting and getting user input
 async function getInput(question){
     
     const rl = readLine.createInterface({
@@ -17,6 +31,7 @@ async function getInput(question){
     const input = await rl.question(question);
     rl.close()
 
+    // resort to default value if no input
     return input.trim() ? input.toString() : "default"
     
 }
@@ -25,40 +40,56 @@ async function getInput(question){
 async function makeFiles(){
 
     try{
-        const defaultHTML = await fs.readFile("./default/fileContents/default.html");
-        const defaultCSS = await fs.readFile("./default/fileContents/default.css");
-        const defaultJS = await fs.readFile("./default/fileContents/default.js");
         
-
-        let nameInput = await getInput("Project name? (default : \"Project\"):\n--> ");
-
+        
+        // request project dir        
         let directoryInput = await getInput("Where do you want to create the project? (default : Desktop):\n--> ");
-                        
-        let contentInput;
+
+        // project's name
+        let nameInput;
+        let projectPath;
 
         do{
-            contentInput = await getInput("Populate with default content?(\"n\" to keep empty):\n--> ");
+            
+            nameInput = await getInput("Project name? (default : \"Project\"):\n--> ");
+            
+            const baseDir = (directoryInput === "default") ? path.join(os.homedir(), 'Desktop') : directoryInput;
+
+            projectPath = path.join(baseDir, nameInput);
+
+            if(await dirExists(projectPath)){
+                console.log(`Project ${projectPath} already exists.`);
+            }
+            
+        }while(await dirExists(projectPath))
+            
+
+        
+        
+        do{
+            contentInput = await getInput("Populate with default content? (\"n\" to keep empty):\n--> ");
             if(contentInput !== "n" && contentInput !== "default"){
                 console.log("Invalid Input");
             }
+
         }while(contentInput !== "n" && contentInput !== "default")
-
-        // const projectDirectory = (directoryInput == "default") ? defaultDir : path.join(directoryInput, folderName);
-
-        const projectName = (nameInput === "default") ? folderName : nameInput;
-
-        const defaultDir = path.join(os.homedir(), 'Desktop', projectName);
-
+            
+            // const projectDirectory = (directoryInput == "default") ? defaultDir : path.join(directoryInput, folderName);
+       
+        // get default content
+        const defaultHTML = await fs.readFile("./default/fileContents/default.html");
+        const defaultCSS = await fs.readFile("./default/fileContents/default.css");
+        const defaultJS = await fs.readFile("./default/fileContents/default.js");
 
         const htmlContent = (contentInput == "default") ? defaultHTML : '';
         const cssContent = (contentInput == "default") ? defaultCSS : '';
         const jsContent = (contentInput == "default") ? defaultJS : '';
 
         await Promise.all([
-            await fs.mkdir(defaultDir, {recursive : true}),
-            await fs.writeFile((path.join(defaultDir, fileNames.HTML)), htmlContent),
-            await fs.writeFile((path.join(defaultDir, fileNames.CSS)), cssContent),
-            await fs.writeFile((path.join(defaultDir, fileNames.JAVASCRIPT)), jsContent)
+            await fs.mkdir(projectPath, {recursive : true}),
+            await fs.writeFile((path.join(projectPath, fileNames.HTML)), htmlContent),
+            await fs.writeFile((path.join(projectPath, fileNames.CSS)), cssContent),
+            await fs.writeFile((path.join(projectPath, fileNames.JAVASCRIPT)), jsContent)
         ]);    
         
 
